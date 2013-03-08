9975d13ee7def33ab4fd0d95da074a80656367ce
Edward G Trenchard <ed_trench@yahoo.com>
2013-03-08 16:47:52 +0000

    08-03-2013

--- EdsBikeRides/Controllers/HomeController.cs
+++ EdsBikeRides/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace EdsBikeRides.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.  ED";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.  ED HELLO WORLD ";
 
             return View();
         }
